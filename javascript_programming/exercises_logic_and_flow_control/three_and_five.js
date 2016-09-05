2c6454fb9b82f1e709d5b201fb7704428808d5b0
David Renz <davidrenz60@gmail.com>
2016-09-05 08:19:20 -0700

    add three and five exercises

---
+++ javascript_programming/exercises_logic_and_flow_control/three_and_five.js
@@ -0,0 +1,21 @@
+function multiplesOfThreeAndFive() {
+  for (var i = 1; i <= 100; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      console.log(i + "!");
+    } else if (i % 3 == 0 || i % 5 == 0) {
+      console.log(i);
+    }
+  }
+}
+
+function multiplesOfThreeAndFive(start, end) {
+  for (var i = start; i <= end; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      console.log(i + "!");
+    } else if (i % 3 == 0 || i % 5 == 0) {
+      console.log(i);
+    }
+  }
+}
+
+multiplesOfThreeAndFive(40, 80);
