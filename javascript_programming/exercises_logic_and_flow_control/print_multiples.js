f880ea70bbf16bfac99d9f27b12c5b38da40afc3
David Renz <davidrenz60@gmail.com>
2016-09-05 14:25:21 -0700

    add print multiples exercises

---
+++ javascript_programming/exercises_logic_and_flow_control/print_multiples.js
@@ -0,0 +1,27 @@
+function printMultiples(num) {
+  for (var i = 100; i >= 0; i--) {
+    if (i % num == 0 && i % 2 == 1) {
+      console.log(i);
+    }
+  }
+}
+
+function printMultiples(num) {
+  var multiple = Math.floor(100 / num) * num;
+
+  while (multiple > 0) {
+    if (multiple % 2 == 1) {
+      console.log(multiple);
+    }
+
+    multiple -= num;
+  }
+}
+
+function printMultiples(num) {
+  for(var multiple = Math.floor(100 / num) * num; multiple > 0; multiple -= num) {
+    if (multiple % 2 == 1) {
+      console.log(multiple);
+    }
+  }
+}
