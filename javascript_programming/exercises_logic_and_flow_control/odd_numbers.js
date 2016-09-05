dde918d6ee486c94f462071aacc4b92b3108789d
David Renz <davidrenz60@gmail.com>
2016-09-05 08:04:41 -0700

    add odd numbers exercise

---
+++ javascript_programming/exercises_logic_and_flow_control/odd_numbers.js
@@ -0,0 +1,24 @@
+function printOddNumbers(num) {
+  for (var i = 1; i <= num; i ++) {
+    if (i % 2 == 1) {
+        console.log(i)
+    }
+  }
+}
+
+
+function printOddNumbers(num) {
+  for (var i = 1; i <= num; i += 2) {
+    console.log(i);
+  }
+}
+
+function printOddNumbers(num) {
+  for (var i = 1; i <= num; i++) {
+    if (i % 2 == 0) {
+      continue;
+    }
+
+    console.log(i) 
+  }
+}
