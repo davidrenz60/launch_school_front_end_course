efffff57dd43bd0b0e6f9c5ef24ca061541753f6
David Renz <davidrenz60@gmail.com>
2016-09-05 14:41:16 -0700

    add fizzbuzz exercises

---
+++ javascript_programming/exercises_logic_and_flow_control/fizzbuzz.js
@@ -0,0 +1,34 @@
+function fizzbuzz() {
+  var message;
+
+  for (var i = 1; i <= 100; i++) {
+    message = i;
+
+    if (i % 3 == 0 && i % 5 == 0) {
+      message = 'FizzBuzz';
+    } else if (i % 3 == 0) {
+      message = 'Fizz';
+    } else if (i % 5 == 0) {
+      message = 'Buzz';
+    }
+
+    console.log(message);
+  }
+}
+
+function fizzbuzz() {
+  var message;
+
+  for (var i = 1; i <= 100; i++) {
+    message = '';
+    if (i % 3 == 0) {
+      message += 'Fizz';
+    }
+
+    if (i % 5 == 0) {
+      message += 'Buzz';
+    }
+
+    console.log(message || i);
+  }
+}
