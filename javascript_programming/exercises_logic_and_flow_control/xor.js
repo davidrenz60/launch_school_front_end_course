baee000cb79a00f99cfa0ba19288e0a5a43597b3
David Renz <davidrenz60@gmail.com>
2016-09-06 06:38:39 -0700

    add xor execise

---
+++ javascript_programming/exercises_logic_and_flow_control/xor.js
@@ -0,0 +1,12 @@
+function isXor(value1, value2) {
+    if (!!value1 === !!value2) {
+        return false;
+    }
+
+    return true;
+}
+
+
+function isXor(value1, value2) {
+  return !!value1 != !!value2
+}
