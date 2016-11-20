c21730f5374268c9966e51fe35c5a044f8d756bc
David Renz <davidrenz60@gmail.com>
2016-11-20 11:11:27 -0800

    enable slideshow prev and next and render info and comments

--- frontend_dev_with_javscript/advanced_jQuery/gallery_server/public/javascripts/gallery.js
+++ frontend_dev_with_javscript/advanced_jQuery/gallery_server/public/javascripts/gallery.js
@@ -15,14 +15,64 @@ $(function() {
     Handlebars.registerPartial($partial.attr('id'), $partial.html());
   });
 
+  // capture slideshow functionality in slideshow object
+  var slideshow = {
+    $el: $('#slideshow'),
+    duration: 500,
+    nextSlide: function(e) {
+      e.preventDefault();
+      var $current = this.$el.find('figure:visible');
+      var $next = $current.next();
+
+      if ($next.length === 0) {
+        $next = this.$el.find('figure').first();
+      }
+
+      $current.fadeOut(this.duration);
+      $next.fadeIn(this.duration);
+      this.renderPhotoContent($next.attr('data-id')); // note: $next.data('id') will coerce into a number
+    },
+
+    prevSlide: function(e) {
+      e.preventDefault();
+      var $current = this.$el.find('figure:visible');
+      var $prev = $current.prev();
+
+      if ($prev.length === 0) {
+        $prev = this.$el.find('figure').last();
+      }
+
+      $current.fadeOut(this.duration);
+      $prev.fadeIn(this.duration);
+      this.renderPhotoContent($prev.attr('data-id'));
+    },
+
+    renderPhotoContent: function(id) {
+      renderPhotoInformation(id);
+      getCommentsFor(id);
+    },
+
+    bindEvents: function() {
+      this.$el.find('a.prev').on('click', this.prevSlide.bind(this));
+      this.$el.find('a.next').on('click', this.nextSlide.bind(this));
+    },
+
+    init: function() {
+      this.bindEvents();
+    },
+  };
+
   // make ajax request and render photos, information and comments of the first photo
   $.ajax({
     url: '/photos',
     success: function(json) {
+      var id;
       photos = json;
+      id = photos[0].id;
       renderPhotos();
-      renderPhotoInformation(0);
-      getCommentsFor(photos[0].id);
+      renderPhotoInformation(id);
+      slideshow.init();
+      getCommentsFor(id);
     },
   });
 
@@ -30,16 +80,21 @@ $(function() {
     $('#slides').html(templates.photos({ photos: photos }));
   }
 
-  function renderPhotoInformation(idx) {
-    $('section > header').html(templates.photo_information(photos[idx]));
+  // change function to find info by id rather than index
+  function renderPhotoInformation(id) {
+    var photo = photos.filter(function(item) {
+      return item.id === +id;
+    })[0];
+
+    $('section > header').html(templates.photo_information(photo));
   }
 
   function getCommentsFor(photoId) {
     $.ajax({
       url: '/comments',
       data: 'photo_id=' + photoId,
-      success: function(comment_json) {
-        $('#comments ul').html(templates.comments({ comments: comment_json }));
+      success: function(commentJson) {
+        $('#comments ul').html(templates.comments({ comments: commentJson }));
       },
     });
   }
