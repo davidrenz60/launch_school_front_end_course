62e432d3aac6ed534b56453e0eca5e582ba9d091
David Renz <davidrenz60@gmail.com>
2017-02-01 20:57:39 -0800

    add backbone exercises

---
+++ client_side_MVC/lesson_2/backbone_models_exercises/javascripts/app.js
@@ -0,0 +1,51 @@
+var PostModel = Backbone.Model.extend({
+  urlRoot: 'http://jsonplaceholder.typicode.com/posts',
+  setUser: function() {
+    var self = this;
+    var user = new UserModel({ id: self.get('userId') });
+
+    user.fetch({
+      success: function(model) {
+        self.set('user', model);
+      }
+    });
+  },
+
+  initialize: function() {
+    this.has('userId') && this.setUser();
+    this.on('change:userId', this.setUser);
+    this.on('change', function(model){
+      model.has('user') && renderPost(model);
+    });
+  },
+});
+
+var UserModel = Backbone.Model.extend({
+  urlRoot: 'http://jsonplaceholder.typicode.com/users',
+});
+
+var post_1 = new PostModel({ id: 1 });
+post_1.fetch();
+
+var post_2 = new PostModel({
+  id: 2,
+  title: 'My New Post',
+  body: 'this is the body',
+  userId: 2,
+});
+
+var post_html = $('#post').html();
+
+function renderPost(model) {
+  var $post = $(post_html);
+  $post.find('h1').text(model.get('title'));
+  $post.find('header p').text(model.get('user').get('name'));
+  $post.find('header + p').text(model.get('body'));
+  $(document.body).html($post);
+}
+
+
+
+
+
+
