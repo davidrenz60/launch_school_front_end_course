c3a7bff0b6827ff72d2ee6ebd72c90c789915216
David Renz <davidrenz60@gmail.com>
2017-02-19 10:26:21 -0800

    initial express project setup

---
+++ client_side_MVC/lesson_6/myapp/routes/all.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router();
+var _ = require('underscore');
+
+module.exports = function(app) {
+  function setActiveNavTo(title) {
+    var activeItem = _(app.locals.links).findWhere({ active: true });
+    if (activeItem) {
+      activeItem.active = false;
+    }
+
+    _(app.locals.links).findWhere({ title: title }).active = true;
+  }
+
+  /* GET home page. */
+  router.get('/', function(req, res, next) {
+    var title = "Home";
+
+    setActiveNavTo(title);
+
+    res.render('index', {
+      title: title,
+    });
+  });
+
+   router.get('/about', function(req, res, next) {
+    var title = "About";
+
+    setActiveNavTo(title);
+
+    res.render('about', {
+      title: title,
+    });
+  });
+
+  return router;
+}
